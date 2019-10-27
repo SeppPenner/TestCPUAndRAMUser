389293cd05f6a0d8d8318f892a6d22081e5c5e90
SeppPenner <HansM2013@gmx.de>
2019-10-27 22:36:05 +0100

    Updated nuget packages, added GitVersionTask.

--- TestCPUAndRAMUser/Properties/AssemblyInfo.cs
+++ TestCPUAndRAMUser/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("99894f6a-717f-410c-821b-20e588bf84b6")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
